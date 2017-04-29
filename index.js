2409d344cc3676194ae31c25c07031a6b21409af
Александр Сапожников (Aleksandr Sapozhnikov) <alexxandrsap@mail.ru>
2017-04-29 21:51:00 +0300

    добавлены примеры деструктуризации

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log(666);
+'use strict';
+
+// деструктуризация массива
+
+{
+  let [firstName, secondName] = ['Василий', 'Задов'];
+  console.log(firstName + ' ' + secondName);
+}
+
+{
+  let [,,secondName] = ['Василий','Петрович','Задов'];
+  console.log(secondName);
+}
+
+{
+  let [a, b, ...spread] = ['запахло', 'гниль', 'в' , 'королевстве', 'датском'];
+  console.log(spread);
+}
+
+{
+  let [firstName = 'Василий', secondName = 'Задов'] = ['Павел'];
+  console.log(firstName + ' ' + secondName);
+}
+
+// деструктуризация объекта
+{
+  let options =
+  {
+    name: 'someName'
+  };
+
+  let {width = 100, height = 50, name: n} = options;
+  console.log(width + ' ' + height + ' ' + n );
+}
